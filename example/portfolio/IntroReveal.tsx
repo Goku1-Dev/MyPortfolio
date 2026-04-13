c234da55616f2f4eb041fe82b30337ccdf693c4e
Goku1-Dev <gokul.sureshkumar06@gmail.com>
2026-04-13 18:22:09 +0530

    Update porfolio logo animation

--- example/portfolio/IntroReveal.tsx
+++ example/portfolio/IntroReveal.tsx
@@ -3,9 +3,12 @@ import { h } from "../../src/index";
 export default function IntroReveal() {
   return (
     <div class="portfolio-intro-overlay">
-       <div class="portfolio-intro-container">
-          <div class="portfolio-intro-shape"></div>
-          <img src="/logo.svg" alt="LogoMorph" class="portfolio-intro-logo" />
+       <div class="portfolio-intro-wrapper">
+         <div class="portfolio-intro-container">
+            <div class="portfolio-intro-shape"></div>
+            <img src="/logo.svg" alt="LogoMorph" class="portfolio-intro-logo" />
+         </div>
+         <div class="portfolio-intro-text">Gokul's Portfolio</div>
        </div>
     </div>
   );
